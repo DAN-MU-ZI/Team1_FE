daab67a3eecf6fc9b7d5b3c8a2950ed026fdd74e
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-08 19:26:49 +0900

    prefix설정

--- src/apis/convertURI.ts
+++ src/apis/convertURI.ts
@@ -1,5 +1,5 @@
 const staticServerUrl_: string = process.env.REACT_APP_PATH || '';
-export const apiURL: string = process.env.REACT_APP_API_URL;
+export const apiURL: string = '/api';
 
 export const convertPath = (path: string): string => {
   return staticServerUrl_ + path;
