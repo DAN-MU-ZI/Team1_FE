a20d95ad37ba6a68e9693c83fd13c26cc05aeb71
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2023-11-08 19:43:00 +0900

    loggin uri

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -12,6 +12,7 @@ const useLogin = (redirectPage?: string) => {
   const loginBtnHandler = (): void => {
     sessionStorage.setItem('beforeLoginURL', redirectPage || '/');
     const redirectURI = baseURL + '/login/kakao';
+    console.log(redirectURI);
     location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_API_KEY}&redirect_uri=${redirectURI}&response_type=code`;
   };
 
